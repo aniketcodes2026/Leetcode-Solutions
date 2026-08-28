05b2b587d1a7084ed63619e0b953f9eace809a2c
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-08-28 19:47:36 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,16 +5,16 @@ public:
         if (n <= 2)
             return n;
 
-        int prev2 = 1;
-        int prev1 = 2;
+        int prev1 = 1;
+        int prev2 = 2;
 
         for (int i = 3; i <= n; i++) {
 
             int current = prev1 + prev2;
 
-            prev2 = prev1;
-            prev1 = current;
+            prev1 = prev2;
+            prev2 = current;
         }
-        return prev1;
+        return prev2;
     }
 };

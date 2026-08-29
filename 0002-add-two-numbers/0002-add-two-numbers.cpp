8e559318c7ed3a496ee8b65ff3371e45ef27708a
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-08-29 12:55:48 +0530

    Time: 0 ms (100%), Space: 77.1 MB (45.91%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+        ListNode* answer = new ListNode(0);
+        ListNode* current = answer;
+
+        int carry = 0;
+
+        while (l1 != NULL || l2 != NULL) {
+
+            int sum = carry;
+
+            if (l1 != NULL) {
+                sum = sum + l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != NULL) {
+                sum = sum + l2->val;
+                l2 = l2->next;
+            }
+
+            if (sum >= 10) {
+                carry = 1;
+                sum = sum - 10;
+            }
+            else {
+                carry = 0;
+            }
+
+            current->next = new ListNode(sum);
+            current = current->next;
+        }
+
+        if (carry == 1) {
+            current->next = new ListNode(1);
+        }
+
+        return answer->next;
+    }
+};

02925c8544423054d0260acab080c886873cdd10
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-09-03 23:11:30 +0530

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {   // O(n^2) solution
+    
+vector<int> twoSum(vector<int>& nums, int target) {   // O(n^2) solution
         for(int i = 0; i < nums.size(); i++) {    // O(n) solution
             for(int j = i + 1; j < nums.size(); j++) {   // O(n) solution
                 if(nums[i] + nums[j] == target) {      // O(1) solution

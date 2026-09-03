c9d1591c7f46d71a2f85168e976fcacd49890026
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-09-03 23:11:41 +0530

    Refactor twoSum function for clarity
    Fix formatting issues and improve readability.

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,6 +5,7 @@ vector<int> twoSum(vector<int>& nums, int target) {   // O(n^2) solution
         for(int i = 0; i < nums.size(); i++) {    // O(n) solution
             for(int j = i + 1; j < nums.size(); j++) {   // O(n) solution
                 if(nums[i] + nums[j] == target) {      // O(1) solution
+                   
                     return {i, j};        // O(1) solution
                 }
             }

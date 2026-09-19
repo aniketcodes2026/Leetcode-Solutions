a31e10bdb65490ceb97a827ea6cf2d5773658267
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-09-19 19:53:46 +0530

    Time: 0 ms (100%), Space: 7.7 MB (68.85%) - LeetHub

--- 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
@@ -4,12 +4,9 @@ public:
                       int x1, int y1, int x2, int y2) {
         
         int x = max(x1, min(xCenter, x2));
-        
         int y = max(y1, min(yCenter, y2));
-
         int a = x - xCenter;
         int b = y - yCenter;
-        
         return a * a + b * b <= radius * radius;
     }
 };

c71e636554e6491ee466185b3de9ece994da02b8
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-09-19 19:53:04 +0530

    Time: 0 ms (100%), Space: 7.8 MB (35%) - LeetHub

---
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter,
+                      int x1, int y1, int x2, int y2) {
+        
+        int x = max(x1, min(xCenter, x2));
+        
+        int y = max(y1, min(yCenter, y2));
+
+        int a = x - xCenter;
+        int b = y - yCenter;
+        
+        return a * a + b * b <= radius * radius;
+    }
+};

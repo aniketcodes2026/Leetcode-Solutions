beca0fe1f70e471cd37db936a16d98f5423c5bba
ANIKET RAJ <aniketvmc2008@gmail.com>
2026-09-21 19:39:21 +0530

    Time: 0 ms (100%), Space: 9.4 MB (28.74%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int mp1[256] = {0};
+        int mp2[256] = {0};
+
+        for (int i = 0; i < s.size(); i++) {
+            char a = s[i];
+            char b = t[i];
+
+            if (mp1[a] != mp2[b])
+                return false;
+
+            mp1[a] = i + 1;
+            mp2[b] = i + 1;
+        }
+
+        return true;
+    }
+};
